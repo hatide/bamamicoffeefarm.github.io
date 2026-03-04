e5e2cbb6b3956fa6f91503cea02380f5aa35608b
Tiến Trần <bamamicoffee@gmail.com>
2026-03-05 00:01:50 +0700

    feat: Optimize Angular build and enable zoneless
    Updates angular.json to disable output hashing and set baseHref to "/".
    Enables zoneless change detection in app.config.ts.
    Updates index.html and docs/404.html to reflect the baseHref change and use static asset names.
    This change aims to improve build performance and potentially enable better caching strategies.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
 import {
   ApplicationConfig,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+  ],
 };
